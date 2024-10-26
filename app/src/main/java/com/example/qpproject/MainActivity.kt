34e16df934cf56ccb8c7c204a7b8cd5be1907bb0
Phil5184 <p3mai@ucsd.edu>
2024-10-26 16:55:20 -0700

    made UI

--- app/src/main/java/com/example/qpproject/MainActivity.kt
+++ app/src/main/java/com/example/qpproject/MainActivity.kt
@@ -13,15 +13,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textBox2:TextView
     private lateinit var button1:Button
     private lateinit var textView2:TextView
+    private lateinit var button2:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         wireWidgets()
-        textBox.text = "Hello"
-        button1.text = "Hello"
+        textBox.text = "Connected Device:"
+        button1.text = "Find My Power Tag"
         textView2.text = "Hello World"
-        textBox2.text = "Hello"
+        textBox2.text = "Power Tag"
+        button2.text = "Current Location"
 
     }
 
@@ -30,5 +32,6 @@ class MainActivity : AppCompatActivity() {
         button1 = findViewById(R.id.button)
         textView2 = findViewById(R.id.textView2)
         textBox2 = findViewById(R.id.textView1)
+        button2 = findViewById(R.id.buttonCurr)
     }
 }
