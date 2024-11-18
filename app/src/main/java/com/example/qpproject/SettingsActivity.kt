6551405876a949cbb214728e4a4d9e4d1d23f882
Phil5184 <p3mai@ucsd.edu>
2024-11-18 01:36:55 -0800

    made UI

--- app/src/main/java/com/example/qpproject/SettingsActivity.kt
+++ app/src/main/java/com/example/qpproject/SettingsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,10 +14,13 @@ import androidx.core.view.WindowInsetsCompat
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var homeButton:Button
-    private lateinit var darkText:TextView
+
     private lateinit var background:ConstraintLayout
     private lateinit var settingsTxt:TextView
-    private var isDark = false
+    private lateinit var darkMode:Switch
+    private lateinit var About:TextView
+    private lateinit var AboutText:TextView
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,33 +35,49 @@ class SettingsActivity : AppCompatActivity() {
 
         homeButton = findViewById(R.id.homeButton1)
         homeButton.text = "Home"
-        darkText = findViewById(R.id.darkMode)
+        darkMode = findViewById(R.id.switch1)
         background = findViewById(R.id.main)
         settingsTxt = findViewById(R.id.SettingsText)
+        About = findViewById(R.id.About1)
+        AboutText = findViewById(R.id.about2)
+
+
+
 
         homeButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
 
         }
-
-        darkText.setOnClickListener {
-            if (!isDark) {
-                background.setBackgroundColor(Color.DKGRAY)
-                darkText.setTextColor(Color.WHITE)
-                settingsTxt.setTextColor(Color.WHITE)
-                darkText.setTextColor(Color.WHITE)
-                isDark = true;
-            }
-            if (isDark) {
-                background.setBackgroundColor(Color.WHITE)
-                darkText.setTextColor(Color.DKGRAY)
-                settingsTxt.setTextColor(Color.DKGRAY)
-                darkText.setTextColor(Color.DKGRAY)
-                isDark = false;
-            }
+        if (darkMode.isChecked) {
+            background.setBackgroundColor(Color.DKGRAY)
+            settingsTxt.setTextColor(Color.WHITE)
+            darkMode.setTextColor(Color.WHITE)
+            About.setTextColor(Color.WHITE)
+            AboutText.setTextColor(Color.WHITE)
+        }
+        else {
+            background.setBackgroundColor(Color.WHITE)
+            settingsTxt.setTextColor(Color.BLACK)
+            darkMode.setTextColor(Color.BLACK)
+            About.setTextColor(Color.BLACK)
+            AboutText.setTextColor(Color.BLACK)
         }
 
-
+        darkMode.setOnCheckedChangeListener { darkMode, isChecked ->
+            if (isChecked) {
+                background.setBackgroundColor(Color.DKGRAY)
+                settingsTxt.setTextColor(Color.WHITE)
+                darkMode.setTextColor(Color.WHITE)
+                About.setTextColor(Color.WHITE)
+                AboutText.setTextColor(Color.WHITE)
+            } else {
+                background.setBackgroundColor(Color.WHITE)
+                settingsTxt.setTextColor(Color.BLACK)
+                darkMode.setTextColor(Color.BLACK)
+                About.setTextColor(Color.BLACK)
+                AboutText.setTextColor(Color.BLACK)
+            }
+        }
     }
 }
